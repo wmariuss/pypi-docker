e174d7947dd14096c44f3ffd1abc0841d5a456d1
root <root@pexbuild>
2019-05-27 14:56:26 +0000

    Update tasks

--- README.md
+++ README.md
@@ -2,3 +2,17 @@
 
 Deploy pypi server for private python libs/packages.
 
+## Requirements
+
+* `Python >= 3.6`
+
+## Install
+
+* Run `pip install invoke`
+* Create `env` file (eg. [env.template](env.template))
+* Run `invoke install --docker --compose`
+* Run `invoke build --pypi` and `invoke run --pypi`
+
+Optional
+
+* Install nginx `invoke nginx --pypi`
--- tasks.py
+++ tasks.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
-
+import os
 from invoke import task, call
 
 
 @task
 def install(c, docker=False, compose=False):
+    '''
+    Install docker and docker-compose
+    '''
     if docker:
         c.run("sudo apt install curl -y")
         c.run("curl -fsSL get.docker.com -o get-docker.sh")
@@ -12,37 +15,59 @@ def install(c, docker=False, compose=False):
         c.run("sudo usermod -aG docker ubuntu")
         c.run("sudo rm get-docker.sh")
     if compose:
-        c.run("sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose")
+        c.run("sudo curl -L https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose")
         c.run("sudo chmod +x /usr/local/bin/docker-compose")
 
 
 @task
 def nginx(c, pypi=False):
+    '''
+    Install nginx and generate self certificate
+    '''
+    ssl_path = '/etc/nginx/ssl'
+
     c.run("sudo apt update")
     c.run("sudo apt install nginx-full -y")
-    c.run("sudo mkdir -p /etc/nginx/ssl")
+    c.run("sudo mkdir -p {}".format(ssl_path))
+    if os.path.exists(ssl_path):
+        c.run('cd {}'.format(ssl_path))
+        c.run('openssl genrsa 2048 > host.key')
+        c.run('openssl req -new -x509 -nodes -sha1 -days 3650 -key host.key > host.cert')
+        c.run('openssl x509 -noout -fingerprint -text < host.cert > host.info')
+        c.run('cat host.cert host.key > host.pem')
+        c.run('chmod 400 host.key host.pem')
     if pypi:
+        c.run('cd')
         c.run("sudo cp nginx/pypi.conf /etc/nginx/conf.d/")
         if True:
-            print("Stopping nginx...")
-            # STOP nginx. We do not have SSL certificate for now.
-            c.run("sudo /bin/systemctl stop nginx.service")
+            print("Restarting nginx...")
+            # restart nginx. We have SSL self certificate for now
+            c.run("sudo /bin/systemctl restart nginx.service")
 
 
 @task
 def build(c, pypi=False):
+    '''
+    docker build
+    '''
     if pypi:
         c.run("sudo docker-compose build")
 
 
 @task(post=[call(nginx, pypi=True)])
 def run(c, pypi=False):
+    '''
+    docker up
+    '''
     if pypi:
         c.run("sudo docker-compose up -d")
 
 
 @task
 def remove(c, containers=False, images=False):
+    '''
+    Clean up
+    '''
     if containers:
         c.run("sudo docker rm $(docker ps -a -q)")
     if images:
